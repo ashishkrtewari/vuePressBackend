b164587922996e852b3061734801bf29b4f99548
Tewari, Ashish <atewari@levi.com>
2018-07-16 05:36:45 +0530

    updated static toute

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import passport from 'passport';
 import './models/userModel';
 import './services/passport';
 import { cookieKey } from "./config/keys";
+import staticRoutes from "./routes/staticRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -41,24 +42,8 @@ app.use(passport.session());
 authRoutes(app);
 postRoutes(app);
 uploadRoutes(app);
+staticRoutes(app);
 
-if (process.env.NODE_ENV === "production") {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static("client/build"));
-
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-else {
-  app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
-}
 
 app.listen(PORT, () => {
   console.log("vuePress running on port :", PORT);
